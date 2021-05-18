6fa51cf0cc8873d8ad279d24805646041362ab7d
Hitman32 <vp.979797@gmail.com>
2021-05-18 16:24:20 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -12,9 +12,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo usermod -aG docker $USER
-newgrp docker
-docker run hello-world
 sleep 10
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
