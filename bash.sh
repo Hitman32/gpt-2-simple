5af0a6a894a83a7ceaab38591352566390e8a226
Hitman32 <vp.979797@gmail.com>
2021-05-18 16:31:18 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -12,13 +12,16 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+docker run hello-world
 sleep 10
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
 sleep 10
-wget https://www.dropbox.com/s/q80vvezt0frmecz/gpt-2-simple.zipclear
+wget https://www.dropbox.com/s/q80vvezt0frmecz/gpt-2-simple.zip
 sudo apt install unzip
 unzip gpt-2-simple.zip
 cd gpt-2-simple
